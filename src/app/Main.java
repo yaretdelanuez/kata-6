df3427e012511912f5b042f62e48bd9849c91080
Yar <Yar@DESKTOP-Yaret>
2020-12-09 16:23:13 +0000

    kata 6 con botones con patron Observer con patrón command terminada

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,12 +1,22 @@
-
 package app;
 
-import app.BlockPanel;
-import java.awt.PopupMenu;
+import control.Command;
+import control.DownCommand;
+import control.LeftCommand;
+import control.RightCommand;
+import control.UpCommand;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.HashMap;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import model.Block;
-
+import view.BlockDisplay;
 
 public class Main extends JFrame {
 
@@ -16,20 +26,23 @@ public class Main extends JFrame {
     }
     private Block block;
     private BlockPanel blockDisplay;
+    private HashMap<String, Command> commands;
     
     public Main(){
         this.setTitle("Block shifter");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(700, 720);
+        this.setSize(700, 762);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
         this.getContentPane().add(blockPanel());
+        this.add(toolbar(), BorderLayout.SOUTH);
     }
     
     private void execute() {
         this.block = new Block(4,4);
         this.blockDisplay.display(block);
         this.block.register(blockDisplay);
+        this.commands = createCommands();
         this.setVisible(true);
         
     }
@@ -38,6 +51,36 @@ public class Main extends JFrame {
         BlockPanel panel = new BlockPanel();
         this.blockDisplay = panel;
         return panel;
+    }
+
+    private HashMap<String, Command> createCommands() {
+        HashMap<String, Command> commands = new HashMap<>();
+        commands.put("L", new LeftCommand(block));
+        commands.put("R", new RightCommand(block));
+        commands.put("U", new UpCommand(block));
+        commands.put("D", new DownCommand(block));
+        return commands;
+    }
+
+    private Component toolbar() {
+        JMenuBar bar = new JMenuBar();
+        bar.setLayout(new FlowLayout(FlowLayout.CENTER));
+        bar.add(button("L"));
+        bar.add(button("R"));
+        bar.add(button("U"));
+        bar.add(button("D"));
+        return bar;
+    }
+
+    private JButton button(String name) {
+        JButton button = new JButton(name);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                commands.get(name).execute();
+            }
+        });
+        return button;
     }
     
 }
